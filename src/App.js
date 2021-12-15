0e7b9e9d1fd1a22d68c55a269e0c819c50dabb36
George Neves <georgeneves97@gmail.com>
2021-12-15 14:41:33 -0300

    This commitment to the content up to class 7 of the React course

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
@@ -21,7 +22,9 @@ function App() {
       name = "rodrigo" 
       idade="28" 
       profissao="Programador" 
-      foto="https://via.placeholder.com/150" />
+      foto="https://via.placeholder.com/150" 
+      />
+      <List />
     </div>
   );
 }
