55a91d1e334093dd8090c138656edd3b586c426e
George Neves <georgeneves97@gmail.com>
2021-12-16 15:49:14 -0300

    feat: added the content of class 15 of the react course

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,23 @@
+import styles from './Footer.module.css'
+import {FaFacebook,FaInstagram,FaLinkedin} from 'react-icons/fa'
+
 function Footer(){
   return(
-    <footer>Rodapé</footer>
+
+    <footer>
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
+      </ul>
+      <p>Nosso rodapé</p>
+    </footer>
   )
 }
 
